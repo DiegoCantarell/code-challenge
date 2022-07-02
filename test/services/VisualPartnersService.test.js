196a91e84f74a9e00529c48105d41390173efd28
kantrax <diegogccantarell@gmail.com>
2022-07-02 18:19:33 -0500

    Step 27: Adding Unit Test 1 for VisualPartnersService class

---
+++ test/services/VisualPartnersService.test.js
@@ -0,0 +1,49 @@
+const VisualPartnersService = require('./../../lib/services/VisualPartnersService.js');
+
+describe('Test for Services -> VisualPartnersService class', () => {
+
+    const visualpartners =    [
+  {
+    "id": "6264d5d89f1df827eb84bb23",
+    "name": "Warren",
+    "email": "Todd@visualpartnership.xyz",
+    "credits": 508,
+    "enrollments": [
+      "Visual Thinking Intermedio",
+      "Visual Thinking Avanzado"
+    ],
+    "previousCourses": 1,
+    "haveCertification": true
+  },
+  {
+    "id": "6264d5d85cf81c496446b67f",
+    "name": "Lucinda",
+    "email": "Sexton@visualpartnership.xyz",
+    "credits": 677,
+    "enrollments": [
+      "Visual Thinking Avanzado"
+    ],
+    "previousCourses": 6,
+    "haveCertification": true
+  },
+  {
+    "id": "6264d5d8cda17de0d2e9f236",
+    "name": "Fuentes",
+    "email": "Sharlene@visualpartnership.xyz",
+    "credits": 210,
+    "enrollments": [
+      "Visual Thinking Avanzado"
+    ],
+    "previousCourses": 10,
+    "haveCertification": true
+  }
+]
+
+    test('#1 Test for getAllStudentsInfo ', () => {
+
+
+        expect(VisualPartnersService.getStudentsEmail(visualpartners)).not.toBeUndefined()
+        expect(VisualPartnersService.getStudentsEmail(visualpartners).length).toBe(3)
+
+    })
+})
