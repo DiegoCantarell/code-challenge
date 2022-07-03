9a40b7acb3d759b9fefa430ea9e725b8e1a98a70
kantrax <diegogccantarell@gmail.com>
2022-07-02 22:29:18 -0500

    Step 31: Adding Unit Test 2 to VisualPartnersController class

--- test/controllers/VisualPartnersController.test.js
+++ test/controllers/VisualPartnersController.test.js
@@ -5,4 +5,9 @@ describe("Test for VisualPartnersController",() => {
         //The amount of students is 51
         expect(VisualPartnersController.getInfo().length).toBe(51)
     })
+    test("#2 Test for getEmails()", ()  => {
+        expect(VisualPartnersController.getEmails()).not.toBeUndefined()
+        //The amount of students with certification is 29
+        expect(VisualPartnersController.getEmails().length).toBe(29)
+    })
 })
