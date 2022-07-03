d97c3db844d2f693b76cf186deb3f38782a7be68
Diego Cantarell <92350521+DiegoCantarell@users.noreply.github.com>
2022-07-02 23:29:21 -0500

    Update Reader.test.js -> changing path

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,7 +2,7 @@ const Reader = require("./../../lib/utils/Reader");
 describe("Unit Test for Reader Class", () => {
 
     test("Create a Reader -> readJsonFile(filePath)", () =>{
-        const path = "explorers.json";
+        const path = "visualpartners.json";
         const result = Reader.readJsonFile(path);
         expect(result).not.toBeUndefined();
     });
